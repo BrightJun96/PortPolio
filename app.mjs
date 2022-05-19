7b1e85e0767702fbc67afee58a11e2f45bf9b4d5
KIM CHEOL JUN <cjfwns966@gmail.com>
2022-05-19 22:01:27 +0900

    전체적인 리팩토링 :  함수형 프로그래밍,코드 줄이기(돔 셀럭터 줄이기)

--- app.mjs
+++ app.mjs
@@ -1,61 +1,78 @@
 /*DOM Selector*/
-const navBar = document.getElementById("navbar");
-const category = document.querySelectorAll(".category");
+
+// DOM ID Selector Function
+function domIDSelector(element) {
+  return document.getElementById(element);
+}
+
+// DOM QuerySelector Function
+function domQuerySelector(selector) {
+  return document.querySelector(selector);
+}
+
+const navBar = domIDSelector("navbar");
+const categorys = document.querySelectorAll(".category");
 
 //버튼
-const moreWatch = document.getElementById("moreWatch");
-const btnScrollToTop = document.getElementById("scrollToTop");
-const logo = document.getElementById("logo");
+const moreWatch = domIDSelector("moreWatch");
+const ScrollToTop = domIDSelector("scrollToTop");
 
 //섹션
-const home = document.getElementById("home");
-const about = document.getElementById("about");
-const skills = document.getElementById("skills");
-const projects = document.getElementById("projects");
-const contact = document.getElementById("contact");
+const home = domIDSelector("home");
+const about = domIDSelector("about");
+const skills = domIDSelector("skills");
+const projects = domIDSelector("projects");
+const contact = domIDSelector("contact");
 
 //각 세션 위치 값
 const navBarHeight = navBar.clientHeight;
-const homeHeight = home.offsetTop - navBarHeight;
-const aboutHeight = about.offsetTop - navBarHeight;
-const skillsHeight = skills.offsetTop - navBarHeight;
-const projectsHeight = projects.offsetTop - navBarHeight;
-const contactHeight = contact.offsetTop - navBarHeight;
+
+// 높이 값을 반환해주는 함수
+function sectionHeight(section) {
+  return section.offsetTop - navBarHeight;
+}
 
 /*menuBar toggle*/
-const menuBar = document.querySelector(".menu-bar");
-const menu = document.querySelector(".menu");
-menuBar.addEventListener("click", (e) => {
+const menuBar = domQuerySelector(".menu-bar");
+const menu = domQuerySelector(".menu");
+
+menuBar.addEventListener("click", () => {
   menu.classList.toggle("visible");
 });
 
 /*scroll function*/
-logo.addEventListener("click", () => {
-  scrollTo(0, homeHeight);
-});
 
 moreWatch.addEventListener("click", () => {
-  scrollTo(0, aboutHeight);
+  scrollTo(0, sectionHeight(about));
 });
 
-btnScrollToTop.addEventListener("click", () => {
-  window.scrollTo(0, 0);
-  console.log(btnScrollToTop);
+ScrollToTop.addEventListener("click", () => {
+  scrollTo(0, 0);
 });
 
-category.forEach((each) => {
-  each.addEventListener("click", (e) => {
-    if (e.target.textContent === home.id) {
-      scrollTo(0, homeHeight);
-    } else if (e.target.textContent === about.id) {
-      scrollTo(0, aboutHeight);
-    } else if (e.target.textContent === skills.id) {
-      scrollTo(0, skillsHeight);
-    } else if (e.target.textContent === projects.id) {
-      scrollTo(0, projectsHeight);
-    } else if (e.target.textContent === contact.id) {
-      scrollTo(0, contactHeight);
-    }
+// 각 세션으로 이동할 수 있게 하는 함수
+function switchScrollToSection(event, sectionName) {
+  switch (event.target.textContent) {
+    case home.id:
+      scrollTo(0, sectionHeight(domIDSelector(sectionName)));
+      break;
+    case about.id:
+      scrollTo(0, sectionHeight(domIDSelector(sectionName)));
+      break;
+    case skills.id:
+      scrollTo(0, sectionHeight(domIDSelector(sectionName)));
+      break;
+    case projects.id:
+      scrollTo(0, sectionHeight(domIDSelector(sectionName)));
+      break;
+    case contact.id:
+      scrollTo(0, sectionHeight(domIDSelector(sectionName)));
+  }
+}
+
+categorys.forEach((category) => {
+  category.addEventListener("click", (event) => {
+    switchScrollToSection(event, category.textContent);
   });
 });
 
@@ -64,15 +81,15 @@ category.forEach((each) => {
 import { modalCotent } from "./util/modalContent.mjs";
 //modal content Selector
 const project = document.querySelectorAll(".each-project");
-const modal = document.querySelector(".modal");
-const modalRemoveBtn = document.querySelector(".modal-removeBtn");
-const projectTitle = document.querySelector(".project-title");
-const projectImg = document.querySelector(".project-img");
-const descriptionConcept = document.querySelector(".description-concept");
-const descriptionContent = document.querySelector(".description-content");
-const descriptionTech = document.querySelector(".description-tech");
-const toLink = document.querySelector(".to-link");
-console.log(toLink.href);
+const modal = domQuerySelector(".modal");
+const modalRemoveBtn = domQuerySelector(".modal-removeBtn");
+const projectTitle = domQuerySelector(".project-title");
+const projectImg = domQuerySelector(".project-img");
+const descriptionConcept = domQuerySelector(".description-concept");
+const descriptionContent = domQuerySelector(".description-content");
+const descriptionTech = domQuerySelector(".description-tech");
+const toLink = domQuerySelector(".to-link");
+
 //put all modal content
 project.forEach((each, index) => {
   each.style.backgroundImage = `url(${modalCotent[index].img})`;
@@ -94,7 +111,7 @@ modalRemoveBtn.addEventListener("click", () => {
 
 /*menu filtering*/
 const filterMenu = document.querySelectorAll(".filter-menu");
-const projectBox = document.getElementById("projectBox");
+const projectBox = domIDSelector("projectBox");
 
 filterMenu.forEach((menu) => {
   menu.addEventListener("click", (e) => {
@@ -103,12 +120,9 @@ filterMenu.forEach((menu) => {
     });
 
     const filterProjectHTML = filterProject.map((each) => {
-      return each.outerHTML; //왜 되지?
+      return each.outerHTML;
     });
 
-    console.log(filterProjectHTML);
-
-    console.log(filterProjectHTML.join(""));
     const allProject = [...project].map((each) => {
       return each.outerHTML;
     });
@@ -119,10 +133,10 @@ filterMenu.forEach((menu) => {
       projectBox.innerHTML = filterProjectHTML.join("");
     }
 
-    [...projectBox.children].forEach((each) => {
-      each.addEventListener("click", (e) => {
+    [...projectBox.children].forEach((project, index) => {
+      console.log(project);
+      project.addEventListener("click", () => {
         modal.classList.add("open-modal");
-        const index = e.currentTarget.dataset.number; //각각 다른 모달 컨텐츠를 보여주기 위해 html의 dataset을 사용하였음.
         projectTitle.textContent = modalCotent[index].projectTitle;
         projectImg.src = modalCotent[index].img;
         descriptionConcept.textContent = modalCotent[index].concept;
